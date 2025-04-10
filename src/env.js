ab4b07ed1fa006d254026f6259844384111d460f
Ivan <info@webapphorizon.com>
2025-04-10 10:22:45 +0300

    Enhance form handling by integrating API key validation and improving error messaging. Added new error messages in Russian content JSON for form submission and newsletter subscription. Refactored actions to utilize dynamic content for better localization and user feedback.

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    FORMCARRY_API_KEY: z.string(),
   },
 
   /**
@@ -17,6 +18,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_YANDEX_MAPS_API_KEY: z.string(),
   },
 
   /**
@@ -26,6 +28,9 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    NEXT_PUBLIC_YANDEX_MAPS_API_KEY:
+      process.env.NEXT_PUBLIC_YANDEX_MAPS_API_KEY,
+    FORMCARRY_API_KEY: process.env.FORMCARRY_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
